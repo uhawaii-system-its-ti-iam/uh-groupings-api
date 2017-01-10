5e8eeb6892290d4e4cc92e6a3df256ab25ff9f81
Zac <zknoebel@hawaii.edu>
2017-01-10 10:02:18 -1000

    modified:   src/main/java/App/GroupingsClient.java

--- src/main/java/App/GroupingsClient.java
+++ src/main/java/App/GroupingsClient.java
@@ -2,8 +2,11 @@ package App;
 
 import APICalls.GrouperFunction;
 import GroupingsFunctions.*;
+import edu.internet2.middleware.grouperClient.api.GcAddMember;
+import edu.internet2.middleware.grouperClient.api.GcDeleteMember;
 import edu.internet2.middleware.grouperClient.ws.beans.*;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,11 +20,13 @@ public class GroupingsClient{
     }
 
     @RequestMapping("/addGrouping")
-    public WsGroupSaveResults addGrouping(String newGrouping){
+    public WsGroupSaveResults addGrouping(@RequestParam(required = true) String newGrouping){
     }
 
     @RequestMapping("/addMember")
-    public WsAddMemberResults addMember(String userToAdd){
+    public WsAddMemberResults addMember(@RequestParam(required = true) String grouping, @RequestParam(required = true) String userToAdd){
+        new GcDeleteMember().assignGroupName(grouping + ":exclude").addSubjectIdentifier(userName).execute();
+        return new GcAddMember().assignGroupName(grouping + ":include").addSubjectIdentifier(userName).execute();
     }
 
     @RequestMapping("/assignOwnership")
