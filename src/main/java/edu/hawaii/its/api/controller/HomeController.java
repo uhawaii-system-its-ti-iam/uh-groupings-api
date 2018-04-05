d1b36070ae73c69acd1dcdd6a5a7cd32c1eb9ff9
zknoebel <zknoebel@hawaii.edu>
2018-04-05 13:31:09 -1000

    remove unused mappings

--- src/main/java/edu/hawaii/its/api/controller/HomeController.java
+++ src/main/java/edu/hawaii/its/api/controller/HomeController.java
@@ -19,22 +19,8 @@ public class HomeController {
     @RequestMapping(value = {"/", "/home"}, method = {RequestMethod.GET})
     public String home(Map<String, Object> model, Locale locale) {
         logger.info("User at home. The client locale is " + locale);
-        return "home";
+        return "/api/groupings/";
     }
-
-    @RequestMapping(value = "/info", method = RequestMethod.GET)
-    public String info(Locale locale, Model model) {
-        logger.info("User at info.");
-        return "info";
-    }
-
-//    @PreAuthorize("hasRole('ADMIN')")
-//    @RequestMapping(value = "/admin", method = RequestMethod.GET)
-//    public String admin(Locale locale, Model model) {
-//        logger.info("User at admin.");
-//        return "admin";
-//    }
-
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Locale locale, Model model) {
