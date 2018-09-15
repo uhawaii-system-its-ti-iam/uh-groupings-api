f61c59ee1decf56ffa1792b392fc7dd11877a5d9
blaine-wataru <blainewataru@gmail.com>
2018-09-14 16:14:01 -1000

    Add MockAdminUser and corresponding factory

---
+++ src/test/java/edu/hawaii/its/api/controller/WithMockAdminSecurityContextFactory.java
@@ -0,0 +1,39 @@
+package edu.hawaii.its.api.controller;
+
+import edu.hawaii.its.api.access.User;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class WithMockAdminSecurityContextFactory
+        implements WithSecurityContextFactory<WithMockAdminUser> {
+
+    @Override
+    public SecurityContext createSecurityContext(WithMockAdminUser adminUser) {
+
+        Set<GrantedAuthority> authorities = new LinkedHashSet<>();
+        for (String role : adminUser.roles()) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+
+        User user = new User(adminUser.username(), authorities);
+        user.setUhuuid(adminUser.uhuuid());
+
+        final Authentication auth =
+                new UsernamePasswordAuthenticationToken(user, "pw", user.getAuthorities());
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(auth);
+
+        return context;
+    }
+
+}
