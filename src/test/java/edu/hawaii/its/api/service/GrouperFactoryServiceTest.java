402d22d490877afd85ce39c927ba388edd4a8728
Nathan Zenger <nzenger@hawaii.edu>
2020-01-23 03:05:29 +0000

    Finish changing all assertEquals tests to assertThat ins API

--- src/test/java/edu/hawaii/its/api/service/GrouperFactoryServiceTest.java
+++ src/test/java/edu/hawaii/its/api/service/GrouperFactoryServiceTest.java
@@ -38,7 +38,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
