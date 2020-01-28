17a17088df4367fdc1b70e3955afe9b53dffafa8
Irene Ma <mairene@hawaii.edu>
2020-01-28 11:14:52 -1000

    update springboot, hibernate, opencsv versions and fix unit tests

--- src/test/java/edu/hawaii/its/api/service/HelperServiceTest.java
+++ src/test/java/edu/hawaii/its/api/service/HelperServiceTest.java
@@ -37,7 +37,7 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { SpringBootWebApplication.class })
 @WebAppConfiguration
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class HelperServiceTest {
 
     @Value("${groupings.api.grouping_admins}")
