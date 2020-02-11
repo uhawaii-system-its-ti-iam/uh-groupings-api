012b1ca8036667eb755be91edc2ac847e7c409f6
johnbigornia <jbigornia15@gmail.com>
2020-02-11 11:48:23 -1000

    varname

--- src/main/java/edu/hawaii/its/api/access/AuthorizationServiceImpl.java
+++ src/main/java/edu/hawaii/its/api/access/AuthorizationServiceImpl.java
@@ -19,10 +19,9 @@ import java.util.Map;
 @Service
 public class AuthorizationServiceImpl implements AuthorizationService {
 
-    @Value("#{'${app.user.roles}'.split(',')}")
-    private List<String> users;
-
     private Map<String, List<Role>> userMap = new HashMap<>();
+
+    //HELLO
 
     @Autowired
     private MemberAttributeService memberAttributeService;
@@ -31,25 +30,6 @@ public class AuthorizationServiceImpl implements AuthorizationService {
     private GroupingAssignmentService groupingAssignmentService;
 
     private static final Log logger = LogFactory.getLog(AuthorizationServiceImpl.class);
-
-    @PostConstruct
-    public void init() {
-        Assert.notNull(users, "property 'app.user.roles' is required.");
-
-        for (String u : users) {
-            String[] uhn = u.split("@");
-            if (uhn.length > 0) {
-                String id = uhn[0];
-                List<Role> roles = new ArrayList<>();
-                if (uhn.length > 1) {
-                    for (String s : uhn[1].split("\\+")) {
-                        roles.add(Role.valueOf(s));
-                    }
-                }
-                userMap.put(id, roles);
-            }
-        }
-    }
 
     /**
      * Assigns roles to user
