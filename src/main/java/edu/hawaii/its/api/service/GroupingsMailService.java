d4f80fb111d986b8c88a81e08b25edd7146db5e7
zakgilbert <gilbertz@hawaii.edu>
2020-02-13 16:57:52 -1000

    Implement basic SMTP service

--- src/main/java/edu/hawaii/its/api/service/GroupingsMailService.java
+++ src/main/java/edu/hawaii/its/api/service/GroupingsMailService.java
@@ -1,4 +1,19 @@
 package edu.hawaii.its.api.service;
 
+import edu.hawaii.its.api.type.GroupingsServiceResult;
+
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
 public interface GroupingsMailService {
+    void setJavaMailSender(JavaMailSender javaMailSender);
+
+    void sendSimpleMessage(String address, String subject, String text);
+
+    void sendAttachmentMessage(String address, String subject, String text, String path,
+            List<GroupingsServiceResult> res)
+            throws MessagingException, IOException;
 }
--- src/main/java/edu/hawaii/its/api/type/GroupingsServiceResult.java
+++ src/main/java/edu/hawaii/its/api/type/GroupingsServiceResult.java
@@ -45,4 +45,5 @@ public class GroupingsServiceResult {
                 + ", person=" + this.person.toString()
                 + "]";
     }
+
 }
