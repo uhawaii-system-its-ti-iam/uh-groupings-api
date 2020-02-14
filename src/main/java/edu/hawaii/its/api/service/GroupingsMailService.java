c144106dfab8f4bfac77e362be15b183c904dc09
zakgilbert <gilbertz@hawaii.edu>
2020-02-14 13:52:50 -1000

    Send email to owner at certain usersToAdd.size()

--- src/main/java/edu/hawaii/its/api/service/GroupingsMailService.java
+++ src/main/java/edu/hawaii/its/api/service/GroupingsMailService.java
@@ -1,39 +1,32 @@
 package edu.hawaii.its.api.service;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import edu.hawaii.its.api.type.GroupingsServiceResult;
 
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 
-import javax.mail.MessagingException;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
-/* Create and send SMTP messages using the JavaMailSender. */
+/**
+ * Create and send SMTP messages using the JavaMailSender.
+ */
 public interface GroupingsMailService {
     void setJavaMailSender(JavaMailSender javaMailSender);
 
     /**
      * Send a SMTP message with no attachment
-     *
-     * @param address - email address to be sent to
-     * @param subject - email subject
-     * @param text    - email text body
      */
     void sendSimpleMessage(String address, String subject, String text);
 
     /**
      * Send an SMTP message with a CSV file attachment.
-     *
-     * @param address - email address to be sent to
-     * @param subject - email subject
-     * @param text    - email text body
-     * @param path    - path or name of the temporary CSV file
-     * @param res     - data to be converted to CSV
      */
+    @Ignore
     void sendCSVMessage(String address, String subject, String text, String path,
             List<GroupingsServiceResult> res);
 
+    /**
+     * Concat UH email suffix onto username
+     */
     String getUserEmail(String username);
 }
