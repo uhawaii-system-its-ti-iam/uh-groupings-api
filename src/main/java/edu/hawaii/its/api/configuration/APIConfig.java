1916b18e06f27c8b6cb9b84e500fce3e758ccf20
nathan <nzenger@hawaii.edu>
2020-03-25 17:46:22 -1000

    Created APIconfig.java to hold the API project properties(Not complete)

---
+++ src/main/java/edu/hawaii/its/api/configuration/APIConfig.java
@@ -0,0 +1,14 @@
+package edu.hawaii.its.api.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix="groupings.api")
+
+public class APIConfig {
+
+
+}
