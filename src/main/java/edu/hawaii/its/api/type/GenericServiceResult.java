aead36ae96a68500501bfa6f9547b80d2189b035
zakgilbert <gilbertz@hawaii.edu>
2020-03-27 16:42:48 -1000

    Comment GenericServiceResult class

--- src/main/java/edu/hawaii/its/api/type/GenericServiceResult.java
+++ src/main/java/edu/hawaii/its/api/type/GenericServiceResult.java
@@ -5,21 +5,40 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Hydrate an object as you see fit. GenericServiceResult is a class which will build a collection of arbitrary objects.
+ */
 public class GenericServiceResult {
-    HashMap<String, Integer> map;
+    // Storage of arbitrary objects.
     ArrayList<Object> data;
+    // Storage of names and indices of each objects added.
+    HashMap<String, Integer> map;
 
     public GenericServiceResult() {
         this.data = new ArrayList<>();
         this.map = new HashMap<>();
     }
 
+    /**
+     * Initialize map and object list then call add.
+     *
+     * @param props   - list of corresponding name values.
+     * @param objects - a variable amount of arbitrary objects.
+     */
     public GenericServiceResult(List<String> props, Object... objects) {
         this.data = new ArrayList<>();
         this.map = new HashMap<>();
         this.add(props, objects);
     }
 
+    /**
+     * Add a variable amount of arbitrary objects, along with a list of their corresponding name values.
+     * Example:
+     * new GenericServiceResult(Arrays.asList("objA", "objB", "objC"), objA, objB, objC );
+     *
+     * @param props   - list of corresponding name values.
+     * @param objects - a variable amount of arbitrary objects.
+     */
     public ArrayList<Object> add(List<String> props, Object... objects) {
         int i = 0;
         for (Object object : objects) {
