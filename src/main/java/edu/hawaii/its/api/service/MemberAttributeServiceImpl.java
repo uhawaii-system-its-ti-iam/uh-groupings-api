973ebe6f8b89ebf3d977b58936d596a96e374ed9
zakgilbert <gilbertz@hawaii.edu>
2020-03-30 14:45:16 -1000

    Use getGroupPaths to determine ownership

--- src/main/java/edu/hawaii/its/api/service/MemberAttributeServiceImpl.java
+++ src/main/java/edu/hawaii/its/api/service/MemberAttributeServiceImpl.java
@@ -502,8 +502,7 @@ public class MemberAttributeServiceImpl implements MemberAttributeService {
         if (isSuperuser(currentUser) || isAdmin(currentUser)) {
             try {
                 return new GenericServiceResult(Arrays.asList("isOwner", "isAdmin"),
-                        (groupingAssignmentService.groupingsIn(groupingAssignmentService.getGroupPaths(
-                                currentUser, usernameInQuestion)).size() > 0), // isOwner?
+                        (groupingAssignmentService.getGroupPaths( currentUser, usernameInQuestion).size() > 0), // isOwner?
                         isAdmin(usernameInQuestion)); //isAdmin?
             } catch (GcWebServiceError e) {
                 logger.error(e, e);
