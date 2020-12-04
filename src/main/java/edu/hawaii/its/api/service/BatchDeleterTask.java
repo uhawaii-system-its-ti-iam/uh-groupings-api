8ea68d673b96102eb7f5bf0adcb5a8e1cc3b58db
Frank Duckart <duckart@computer.org>
2020-12-03 15:05:15 -1000

    Remove unused imports.

--- src/main/java/edu/hawaii/its/api/service/BatchDeleterTask.java
+++ src/main/java/edu/hawaii/its/api/service/BatchDeleterTask.java
@@ -1,33 +1,26 @@
 package edu.hawaii.its.api.service;
 
-import edu.internet2.middleware.grouperClient.ws.beans.WsDeleteMemberResults;
-
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+
+import edu.internet2.middleware.grouperClient.ws.beans.WsDeleteMemberResults;
 
 public class BatchDeleterTask implements Callable<WsDeleteMemberResults> {
 
-        private final int currGroup;
-        private final String userToRemove;
-        private final List<String> groupPaths;
-        private final BatchDeleter bd;
+    private final int currGroup;
+    private final String userToRemove;
+    private final List<String> groupPaths;
+    private final BatchDeleter bd;
 
-        public BatchDeleterTask(int currGroup, String userToRemove, List<String> groupPaths, BatchDeleter bd){
-            this.currGroup = currGroup;
-            this.userToRemove = userToRemove;
-            this.groupPaths = groupPaths;
-            this.bd = bd;
-        }
+    public BatchDeleterTask(int currGroup, String userToRemove, List<String> groupPaths, BatchDeleter bd) {
+        this.currGroup = currGroup;
+        this.userToRemove = userToRemove;
+        this.groupPaths = groupPaths;
+        this.bd = bd;
+    }
 
-        @Override
-        public WsDeleteMemberResults call(){
-                return bd.makeWsDeleteMemberResults(groupPaths.get(currGroup), userToRemove);
-        }
-
+    @Override
+    public WsDeleteMemberResults call() {
+        return bd.makeWsDeleteMemberResults(groupPaths.get(currGroup), userToRemove);
+    }
 }
