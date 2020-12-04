9f620b1e665986ce76091673da09e9af8a8ad341
Frank Duckart <duckart@computer.org>
2020-12-03 15:02:25 -1000

    Testing update.

--- src/test/java/edu/hawaii/its/api/service/MembershipServiceUnitTest.java
+++ src/test/java/edu/hawaii/its/api/service/MembershipServiceUnitTest.java
@@ -1,9 +1,12 @@
 package edu.hawaii.its.api.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -39,6 +42,72 @@ public class MembershipServiceUnitTest {
 
         //Validates that every returned result is a memberResult, proving all of the threads are working properly.
         memberResults.forEach(
-                result -> assertTrue((result.toString()).contains("edu.internet2.middleware.grouperClient.ws.beans.WsDeleteMemberResults@")));
+                result -> assertTrue((result.toString())
+                        .contains("edu.internet2.middleware.grouperClient.ws.beans.WsDeleteMemberResults@")));
     }
+
+    @Test
+    public void batchDeleteTwo() {
+        final int TEST_RUNS = 25;
+        final Map<Integer, Integer> map = new HashMap<>();
+
+        Thread[] serviceThreads = new DeleteTestRunner[TEST_RUNS];
+        for (int i = 0; i < TEST_RUNS; i++) {
+            serviceThreads[i] = new DeleteTestRunner(i, map);
+            serviceThreads[i].start();
+        }
+
+        // Wait on the threads to finish.
+        pollUntilFinished(TEST_RUNS, serviceThreads);
+
+        // Check the results.
+        for (int i = 0; i < TEST_RUNS; i++) {
+            assertEquals(map.get(i).intValue(), i + 100);
+        }
+    }
+
+    private synchronized void pollUntilFinished(final int TEST_RUNS, Thread[] serviceThreads) {
+        // Poll the threads to see if they are done.
+        boolean isFinished = false;
+        while (!isFinished) {
+            isFinished = true;
+            for (int i = 0; i < TEST_RUNS; i++) {
+                if (serviceThreads[i].isAlive()) {
+                    sleep(50);
+                    isFinished = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void sleep(long milliSeconds) {
+        try {
+            Thread.sleep(milliSeconds);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted during sleep.");
+        }
+    }
+
+    private class DeleteTestRunner extends Thread {
+        private final int id;
+        private final Map<Integer, Integer> map;
+
+        public DeleteTestRunner(int id, Map<Integer, Integer> map) {
+            this.id = id;
+            this.map = map;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("Running " + id);
+                map.put(id, id + 100);
+                // Remove above and put batch delete call.
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
 }
