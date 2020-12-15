8ff5e156d282f3598df2b81b5ef9483a1f4ddc95
nathan <nzenger@hawaii.edu>
2020-12-14 14:57:27 -1000

    Added more classes to BatchIsMemberTask and increase optIn speed with more thread classes.

--- src/main/java/edu/hawaii/its/api/service/BatchIsMember.java
+++ src/main/java/edu/hawaii/its/api/service/BatchIsMember.java
@@ -4,4 +4,6 @@ import edu.hawaii.its.api.type.Person;
 
 public interface BatchIsMember {
     boolean isMember(String groupPath, Person personToAdd);
+    boolean isOwner(String groupPath, String personToAdd);
+    boolean isSuperuser(String username);
 }
--- src/main/java/edu/hawaii/its/api/service/BatchIsMemberTask.java
+++ src/main/java/edu/hawaii/its/api/service/BatchIsMemberTask.java
@@ -20,3 +20,36 @@ public class BatchIsMemberTask implements Callable<Boolean> {
         return bd.isMember(groupPath, personToAdd);
     }
 }
+
+class BatchIsOwnerTask implements Callable<Boolean> {
+
+    private final String groupPath;
+    private final String personToAdd;
+    private final BatchIsMember bd;
+
+    public BatchIsOwnerTask(String groupPath, String personToAdd, BatchIsMember bd) {
+        this.groupPath = groupPath;
+        this.personToAdd = personToAdd;
+        this.bd = bd;
+    }
+
+    @Override
+    public Boolean call() {
+        return bd.isOwner(groupPath, personToAdd);
+    }
+}
+
+class BatchIsSuperUserTask implements Callable<Boolean> {
+    private final String username;
+    private final BatchIsMember bd;
+
+    public BatchIsSuperUserTask(String username, BatchIsMember bd) {
+        this.username = username;
+        this.bd = bd;
+    }
+
+    @Override
+    public Boolean call() {
+        return bd.isSuperuser(username);
+    }
+}
