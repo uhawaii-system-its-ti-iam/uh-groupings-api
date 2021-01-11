f7363aae6645b1bbdd8db71c4ebe749eeace6ce1
zakgilbert <gilbertz@hawaii.edu>
2021-01-11 09:08:36 -1000

    Create AddMemberResult type class.

---
+++ src/main/java/edu/hawaii/its/api/type/AddMemberResult.java
@@ -0,0 +1,41 @@
+package edu.hawaii.its.api.type;
+
+public class AddMemberResult {
+    boolean userWasAdded;
+    boolean userWasRemoved;
+    String pathOfAdd;
+    String pathOfRemoved;
+    String name;
+    String uhUuid;
+    String uid;
+    String result;
+
+    public AddMemberResult() {
+    }
+
+    public AddMemberResult(boolean userWasAdded, boolean userWasRemoved, String pathOfAdd, String pathOfRemoved,
+            String name, String uhUuid, String uid, String result) {
+        this.userWasAdded = userWasAdded;
+        this.userWasRemoved = userWasRemoved;
+        this.pathOfAdd = pathOfAdd;
+        this.pathOfRemoved = pathOfRemoved;
+        this.name = name;
+        this.uhUuid = uhUuid;
+        this.uid = uid;
+        this.result = result;
+    }
+    public boolean isUserWasAdded() { return userWasAdded; }
+    public  boolean isUserWasRemoved() { return userWasRemoved; }
+    public String getPathOfAdd(){return pathOfAdd;}
+    public void setPathOfAdd(String pathOfAdd) { this.pathOfAdd = pathOfAdd; }
+    public String getPathOfRemoved() {return pathOfRemoved;}
+    public void setPathOfRemoved(String pathOfRemoved) { this.pathOfRemoved = pathOfRemoved; }
+    public String getName() {return name;}
+    public void setName(String name) { this.name = name; }
+    public String getUhUuid(){return uhUuid;}
+    public void setUhUuid(String uhUuid) { this.uhUuid = uhUuid; }
+    public String getUid(){return uid;}
+    public void setUid(String uid) { this.uid = uid; }
+    public String getResult() { return result; }
+    public void setResult(String result) { this.result = result; }
+}
