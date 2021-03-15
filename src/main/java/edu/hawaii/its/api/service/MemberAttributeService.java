b2d0426b91d55caaeed5cd7e1b2be1ff7b564fea
zakgilbert <gilbertz@hawaii.edu>
2021-03-15 11:25:42 -1000

    Fix codacy errors.

--- src/main/java/edu/hawaii/its/api/service/MemberAttributeService.java
+++ src/main/java/edu/hawaii/its/api/service/MemberAttributeService.java
@@ -4,7 +4,6 @@ import edu.hawaii.its.api.type.GroupingPath;
 import edu.hawaii.its.api.type.GroupingsServiceResult;
 import edu.hawaii.its.api.type.Person;
 
-import edu.internet2.middleware.grouperClient.ws.GcWebServiceError;
 import edu.internet2.middleware.grouperClient.ws.beans.WsAttributeAssign;
 
 import java.util.List;
