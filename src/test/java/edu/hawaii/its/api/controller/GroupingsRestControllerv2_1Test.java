1c75d819a6c054fefa88b2a98f42021ade96cd06
Chak Hon <chakhon@hawaii.edu>
2021-08-17 12:09:12 -1000

    removed unused import

--- src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerv2_1Test.java
+++ src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerv2_1Test.java
@@ -51,7 +51,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
