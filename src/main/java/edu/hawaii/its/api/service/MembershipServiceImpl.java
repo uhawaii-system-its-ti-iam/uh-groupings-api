bf41c926e5c60aec49fecb3bbf378259927c5cd5
ruizaj <60155925+ruizaj@users.noreply.github.com>
2021-11-23 08:32:31 -1000

    Update MembershipServiceImpl.java

--- src/main/java/edu/hawaii/its/api/service/MembershipServiceImpl.java
+++ src/main/java/edu/hawaii/its/api/service/MembershipServiceImpl.java
@@ -255,7 +255,6 @@ public class MembershipServiceImpl implements MembershipService {
             if (!membership.isInExclude() && !membership.isInBasis()) {
                 memberships.add(membership);
             }
-            //memberships.add(membership);
         }
         return memberships;
     }
