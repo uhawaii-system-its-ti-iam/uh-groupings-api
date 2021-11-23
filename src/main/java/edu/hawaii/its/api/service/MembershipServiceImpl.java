911bb1f25ce863cd11ba16fc7adb32ea6548b568
ruizaj <60155925+ruizaj@users.noreply.github.com>
2021-11-23 08:23:35 -1000

    Changed membershipResults test to accomadate new changes

--- src/main/java/edu/hawaii/its/api/service/MembershipServiceImpl.java
+++ src/main/java/edu/hawaii/its/api/service/MembershipServiceImpl.java
@@ -255,6 +255,7 @@ public class MembershipServiceImpl implements MembershipService {
             if (!membership.isInExclude() && !membership.isInBasis()) {
                 memberships.add(membership);
             }
+            //memberships.add(membership);
         }
         return memberships;
     }
