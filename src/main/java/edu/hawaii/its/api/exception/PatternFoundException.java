7b8cf03c80352a620d5ed3e7fcb0a20223fee57a
carol <97712287+carolwong492@users.noreply.github.com>
2022-07-12 13:24:41 -1000

    Updates PasswordScanner and adds its tests (#331)

---
+++ src/main/java/edu/hawaii/its/api/exception/PatternFoundException.java
@@ -0,0 +1,8 @@
+package edu.hawaii.its.api.exception;
+
+public class PatternFoundException extends RuntimeException {
+
+    public PatternFoundException(String message) {
+        super(message);
+    }
+}
