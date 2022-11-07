42134dd75ba889fb22137fdb35b079576b3f9633
lukepag <87156222+lukepag@users.noreply.github.com>
2022-11-07 13:27:54 -1000

    Add OptType object to enable/disablePreferences parameters (#385)
    Fixed converter
    
    Improve enable/disablePreferenceTest

---
+++ src/main/java/edu/hawaii/its/api/configuration/WebConfig.java
@@ -0,0 +1,14 @@
+package edu.hawaii.its.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToOptTypeConverter());
+    }
+}
