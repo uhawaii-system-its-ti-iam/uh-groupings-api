aa116d85a028250004ffb1ec09b2980eb608872d
carol <97712287+carolwong492@users.noreply.github.com>
2023-01-24 13:24:21 -1000

    Readded thymeleaf dependency and test (#405)

--- src/test/java/edu/hawaii/its/api/controller/HomeControllerTest.java
+++ src/test/java/edu/hawaii/its/api/controller/HomeControllerTest.java
@@ -1,18 +1,21 @@
 package edu.hawaii.its.api.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import edu.hawaii.its.api.configuration.SpringBootWebApplication;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import edu.hawaii.its.api.configuration.SpringBootWebApplication;
 
 @SpringBootTest(classes = { SpringBootWebApplication.class })
 public class HomeControllerTest {
@@ -29,8 +32,11 @@ public class HomeControllerTest {
 
     @Test
     public void requestHome() throws Exception {
-        mockMvc.perform(get("/"))
+        MvcResult result = mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("home"));
+                .andExpect(view().name("home"))
+                .andReturn();
+        String pagetext = result.getResponse().getContentAsString();
+        assertThat(pagetext, containsString("This is the API side of UH Groupings"));
     }
 }
