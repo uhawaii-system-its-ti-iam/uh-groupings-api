af6c41a71f9449713240cc2360d878fd148dbc58
Jarren Seson <jarrenys@hawaii.edu>
2023-06-15 13:30:29 -1000

    Deletes localHost from @Profile('localHost')
    Deletes @Profile(localTest) from GroupAttributeTest.java
    
    Deletes localHost from @Profile('localHost')

--- src/test/java/edu/hawaii/its/api/wrapper/GroupAttributeTest.java
+++ src/test/java/edu/hawaii/its/api/wrapper/GroupAttributeTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ActiveProfiles("localTest")
 @SpringBootTest(classes = { SpringBootWebApplication.class })
 public class GroupAttributeTest {
     private PropertyLocator propertyLocator;
