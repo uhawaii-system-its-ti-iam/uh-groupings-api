799760e295425553b7ba2d763684301ffef8566f
Jordan Wong <42422209+JorWo@users.noreply.github.com>
2023-06-19 13:13:08 -1000

    Combine enable/disablePreference into one method (#461)

---
+++ src/test/java/edu/hawaii/its/api/configuration/StringToOptTypeConverterTest.java
@@ -0,0 +1,33 @@
+package edu.hawaii.its.api.configuration;
+
+import edu.hawaii.its.api.type.OptType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class StringToOptTypeConverterTest {
+
+    StringToOptTypeConverter stringToOptTypeConverter = new StringToOptTypeConverter();
+
+    @Test
+    public void convertInvalidOptTypeTest() {
+        OptType optType = stringToOptTypeConverter.convert("invalid");
+        assertNull(optType);
+    }
+
+    @Test
+    public void convertOptTypeInTest() {
+        OptType optType = stringToOptTypeConverter.convert(OptType.IN.value());
+        assertNotNull(optType);
+        assertEquals(OptType.IN, optType);
+    }
+
+    @Test
+    public void convertOptTypeOutTest() {
+        OptType optType = stringToOptTypeConverter.convert(OptType.OUT.value());
+        assertNotNull(optType);
+        assertEquals(OptType.OUT, optType);
+    }
+}
