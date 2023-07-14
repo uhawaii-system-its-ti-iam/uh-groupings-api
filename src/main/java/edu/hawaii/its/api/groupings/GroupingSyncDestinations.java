db21131c159e7e7018f6ae65b78dc1e070d7accb
Jordan Wong <42422209+JorWo@users.noreply.github.com>
2023-07-14 11:27:41 -1000

    Sort sync destinations (#483)
    Co-authored-by: Trey <treyyy@hawaii.edu>

--- src/main/java/edu/hawaii/its/api/groupings/GroupingSyncDestinations.java
+++ src/main/java/edu/hawaii/its/api/groupings/GroupingSyncDestinations.java
@@ -3,10 +3,12 @@ package edu.hawaii.its.api.groupings;
 import edu.hawaii.its.api.util.JsonUtil;
 import edu.hawaii.its.api.wrapper.AttributesResult;
 import edu.hawaii.its.api.wrapper.FindAttributesResults;
+import edu.hawaii.its.api.wrapper.Group;
 import edu.hawaii.its.api.wrapper.GroupAttribute;
 import edu.hawaii.its.api.wrapper.GroupAttributeResults;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -16,9 +18,11 @@ public class GroupingSyncDestinations {
     private String resultCode;
     private List<GroupingSyncDestination> syncDestinations;
     private List<GroupAttribute> groupAttributes;
+    private String groupingExtension;
 
     public GroupingSyncDestinations(FindAttributesResults findAttributesResults,
             GroupAttributeResults groupAttributeResults) {
+        setGroupingExtension(groupAttributeResults.getGroups());
         setGroupAttributes(groupAttributeResults.getGroupAttributes());
         setSyncDestination(findAttributesResults.getResults());
         setResultCode(findAttributesResults.getResultCode());
@@ -26,6 +30,7 @@ public class GroupingSyncDestinations {
     }
 
     public GroupingSyncDestinations() {
+        setGroupingExtension(new ArrayList<>());
         setGroupAttributes(new ArrayList<>());
         setSyncDestination(new ArrayList<>());
         setResultCode("FAILURE");
@@ -49,13 +54,20 @@ public class GroupingSyncDestinations {
             GroupingSyncDestination groupingSyncDestination =
                     JsonUtil.asObject(attributesResult.getDescription(), GroupingSyncDestination.class);
             groupingSyncDestination.setName(attributesResult.getName());
+            groupingSyncDestination.setDescription(groupingSyncDestination.getDescription()
+                    .replaceFirst("\\$\\{srhfgs}", this.groupingExtension));
             groupingSyncDestination.setSynced(this.groupAttributes.stream()
                     .anyMatch(groupAttribute -> groupAttribute.getAttributeName().equals(attributesResult.getName())));
             this.syncDestinations.add(groupingSyncDestination);
         }
+        this.syncDestinations.sort(Comparator.comparing(GroupingSyncDestination::getDescription));
     }
 
     private void setGroupAttributes(List<GroupAttribute> groupAttributes) {
         this.groupAttributes = groupAttributes;
+    }
+
+    private void setGroupingExtension(List<Group> groups) {
+        this.groupingExtension = !groups.isEmpty() ? groups.get(0).getExtension() : "";
     }
 }
--- src/test/java/edu/hawaii/its/api/service/TestGroupingOwnerService.java
+++ src/test/java/edu/hawaii/its/api/service/TestGroupingOwnerService.java
@@ -6,15 +6,21 @@ import edu.hawaii.its.api.configuration.SpringBootWebApplication;
 import edu.hawaii.its.api.groupings.GroupingOptAttributes;
 import edu.hawaii.its.api.groupings.GroupingDescription;
 import edu.hawaii.its.api.groupings.GroupingGroupsMembers;
+import edu.hawaii.its.api.groupings.GroupingSyncDestination;
 import edu.hawaii.its.api.groupings.GroupingSyncDestinations;
+import edu.hawaii.its.api.type.Grouping;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ActiveProfiles("integrationTest")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -82,6 +88,15 @@ public class TestGroupingOwnerService {
         assertNotNull(groupingSyncDestinations);
         assertEquals(SUCCESS, groupingSyncDestinations.getResultCode());
         assertNotNull(groupingSyncDestinations.getSyncDestinations());
+
+        assertEquals(groupingSyncDestinations.getSyncDestinations(),
+                groupingSyncDestinations.getSyncDestinations().stream()
+                        .sorted(Comparator.comparing(GroupingSyncDestination::getDescription))
+                        .collect(Collectors.toList()));
+
+        assertTrue(groupingSyncDestinations.getSyncDestinations().stream()
+                .filter(syncDestination -> !syncDestination.getDescription().contains("uhReleasedGrouping"))
+                .allMatch(e -> e.getDescription().contains(new Grouping(GROUPING).getName())));
     }
 
 }
