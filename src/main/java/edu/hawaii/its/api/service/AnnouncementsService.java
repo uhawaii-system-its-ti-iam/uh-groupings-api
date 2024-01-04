1edd6aba019ca86e2cb018d1eb50f7adc11718ba
Michelle Ho <89873670+michho8@users.noreply.github.com>
2024-01-03 14:40:58 -1000

    Implement the "announcements" attributes from Grouper (#509)

---
+++ src/main/java/edu/hawaii/its/api/service/AnnouncementsService.java
@@ -0,0 +1,44 @@
+package edu.hawaii.its.api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import edu.hawaii.its.api.type.Announcement;
+import edu.hawaii.its.api.type.Announcements;
+import edu.hawaii.its.api.util.JsonUtil;
+import edu.hawaii.its.api.wrapper.AttributesResult;
+import edu.hawaii.its.api.wrapper.FindAttributesResults;
+
+@Service
+public class AnnouncementsService {
+    public static final Log log = LogFactory.getLog(AnnouncementsService.class);
+
+    @Value("${groupings.api.announcements}")
+    private String ANNOUNCEMENTS_ATTR_NAME;
+
+    @Value("${groupings.api.propertystring}")
+    private String ANNOUNCEMENTS_ATTR_DEF;
+
+    @Autowired
+    private GrouperApiService grouperApiService;
+
+    public Announcements setAnnouncements(AttributesResult attributesResult) {
+        List<Announcement> announcementsList = new ArrayList<>();
+        announcementsList = JsonUtil.asList(attributesResult.getDescription(), Announcement.class);
+        return new Announcements(announcementsList);
+    }
+
+    public List<String> activeAnnouncements() {
+        FindAttributesResults findAttributesResults = grouperApiService.findAttributesResults(
+                ANNOUNCEMENTS_ATTR_DEF,
+                ANNOUNCEMENTS_ATTR_NAME);
+        Announcements activeAnnouncements = setAnnouncements(findAttributesResults.getResult());
+        return activeAnnouncements.validMessages(activeAnnouncements.getAnnouncements());
+    }
+}
---
+++ src/main/java/edu/hawaii/its/api/type/Announcements.java
@@ -0,0 +1,45 @@
+package edu.hawaii.its.api.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+@Service
+public class Announcements {
+    private String resultCode;
+    private List<Announcement> announcements = new ArrayList<>();
+
+    public Announcements(List<Announcement> announcements) {
+        if (announcements != null) {
+            for (Announcement a : announcements) {
+                this.announcements.add(a);
+            }
+            setResultCode("SUCCESS");
+        }
+    }
+
+    public Announcements() {
+        setResultCode("FAILURE");
+    }
+
+    private void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public List<Announcement> getAnnouncements() {
+        return announcements;
+    }
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public List<String> validMessages(List<Announcement> allGroupingsAnnouncements) {
+        List<String> validMessages = new ArrayList<>();
+        for (Announcement groupingsAnnouncement : allGroupingsAnnouncements) {
+            if (groupingsAnnouncement.getState() == Announcement.State.Active) {
+                validMessages.add(groupingsAnnouncement.getMessage());
+            }
+        }
+        return validMessages;
+    }
+}
