5d3ca3bb5d7fbf0b4ff8121a1302f6dd95952731
Jordan Wong <jordanw4@hawaii.edu>
2024-01-19 18:19:41 -1000

    Refactor Announcements and AnnouncementsService

--- src/test/java/edu/hawaii/its/api/service/TestAnnouncementsService.java
+++ src/test/java/edu/hawaii/its/api/service/TestAnnouncementsService.java
@@ -3,8 +3,6 @@ package edu.hawaii.its.api.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,40 +12,22 @@ import org.springframework.test.context.ActiveProfiles;
 
 import edu.hawaii.its.api.configuration.SpringBootWebApplication;
 import edu.hawaii.its.api.type.Announcements;
-import edu.hawaii.its.api.wrapper.FindAttributesResults;
 
 @ActiveProfiles("integrationTest")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(classes = { SpringBootWebApplication.class })
 public class TestAnnouncementsService {
-    @Value("${groupings.api.announcements}")
-    private String ANNOUNCEMENTS_ATTR_NAME;
-    @Value("${groupings.api.propertystring}")
-    private String ANNOUNCEMENTS_ATTR_DEF;
     @Value("${groupings.api.success}")
     private String SUCCESS;
     @Autowired
     private AnnouncementsService announcementsService;
-    @Autowired
-    private GrouperApiService grouperApiService;
 
     @Test
-    public void setAnnouncements() {
-        FindAttributesResults findAttributesResults = grouperApiService.findAttributesResults(
-                ANNOUNCEMENTS_ATTR_DEF,
-                ANNOUNCEMENTS_ATTR_NAME);
-        assertNotNull(findAttributesResults.getResults());
-        Announcements results = announcementsService.setAnnouncements(findAttributesResults.getResult());
-        assertNotNull(results);
-        assertNotNull(results.getAnnouncements());
-        assertEquals(SUCCESS, results.getResultCode());
+    public void getAnnouncementsTest() {
+        Announcements announcements = announcementsService.getAnnouncements();
+        assertNotNull(announcements);
+        assertNotNull(announcements.getAnnouncements());
+        assertEquals(SUCCESS, announcements.getResultCode());
     }
 
-    @Test
-    public void activeAnnouncements() {
-        List<String> results = announcementsService.activeAnnouncements();
-        // Only when Grouper has this case specific data (will need to keep updating).
-        assertNotNull(results);
-        assertEquals("Test is now running on VMs featuring Java 17 (hello Spring Boot3)", results.get(0));
-    }
 }
