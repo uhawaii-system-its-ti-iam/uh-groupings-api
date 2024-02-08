61316370699f79719dc2bebf2bf0396257be495b
gitCarrot <jeonhg0@gmail.com>
2024-02-08 12:54:30 -1000

    Create Service for getting wrapper class object from the bean that has binded data from data harness

--- src/main/java/edu/hawaii/its/api/service/GroupingPropertiesService.java
+++ src/main/java/edu/hawaii/its/api/service/GroupingPropertiesService.java
@@ -1,6 +1,9 @@
 package edu.hawaii.its.api.service;
 
+import edu.hawaii.its.api.wrapper.AssignAttributesResults;
 import edu.hawaii.its.api.wrapper.FindGroupsResults;
+import edu.hawaii.its.api.wrapper.GetMembersResults;
+import edu.hawaii.its.api.wrapper.GroupSaveResults;
 import edu.hawaii.its.api.wrapper.HasMembersResults;
 import edu.hawaii.its.api.wrapper.SubjectsResults;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,21 +30,34 @@ public class GroupingPropertiesService {
     @Qualifier("GetSubjectsResultsOOTBBean")
     private SubjectsResults subjectsResults;
 
+    @Autowired
+    @Qualifier("GroupSaveResultsOOTBBean")
+    private GroupSaveResults groupSaveResults;
+
+    @Autowired
+    @Qualifier("AssignAttributesOOTBBean")
+    private AssignAttributesResults assignAttributesResults;
+
+    @Autowired
+    @Qualifier("GetMembersResultsOOTBBean")
+    private GetMembersResults getMembersResults;
+
     public GrouperService getGrouperService() {
         GrouperService grouperService1 = grouperService;
         return grouperService1;
     }
     public HasMembersResults getHasMembersResultsBean() {
-        HasMembersResults hasMembersResults1 = hasMembersResults;
-        return hasMembersResults1;
+        return this.hasMembersResults;
     }
-
     public FindGroupsResults getFindGroupsResults() {
         return this.findGroupsResults;
     }
-
     public SubjectsResults getSubjectsResults(){
         return this.subjectsResults;
     }
+    public GroupSaveResults  getGroupSaveResults() { return this.groupSaveResults; }
+    public AssignAttributesResults getAssignAttributesResults() { return this.assignAttributesResults; }
+    public GetMembersResults getMembersResults() { return this.getMembersResults; }
+
 }
 
