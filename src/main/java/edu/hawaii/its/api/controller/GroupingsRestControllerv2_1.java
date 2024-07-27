877a81035f602e15809688c79583497b281dd718
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2024-07-26 17:23:38 -1000

    Create an endpoint to check if path is valid

--- src/main/java/edu/hawaii/its/api/controller/GroupingsRestControllerv2_1.java
+++ src/main/java/edu/hawaii/its/api/controller/GroupingsRestControllerv2_1.java
@@ -128,6 +128,17 @@ public class GroupingsRestControllerv2_1 {
     }
 
     /**
+     * Check if a grouping path is valid.
+     */
+    @GetMapping(value = "/grouping/{path:[\\w-:.]+}/is-valid")
+    @ResponseBody
+    public ResponseEntity<Boolean> groupingPathIsValid(@PathVariable String path) {
+        return ResponseEntity
+                .ok()
+                .body(groupingAttributeService.isGroupingPath(path));
+    }
+
+    /**
      * Get a list of all admins.
      */
     @GetMapping(value = "/grouping-admins")
