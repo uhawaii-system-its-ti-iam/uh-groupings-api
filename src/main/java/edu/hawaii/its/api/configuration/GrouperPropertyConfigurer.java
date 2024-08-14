9c3bf10df0766e00565fbd7beb76e54919afb68a
Michael Hodges <mhodgesatuh@users.noreply.github.com>
2024-08-13 19:51:22 -1000

    Add dockerhost properties file (#576)
    * Add Docker and Vault support
    
    When running the project from the supported docker containers the Grouper API password will be retrieved from the vault instead of the overrides file.
    
    * Fix mapping to the overrides file
    
    Fix mapping to the overrides file
    
    * Update dockerhost properties file
    
    * Fix spring.config.import in dockerhost.properties

--- src/main/java/edu/hawaii/its/api/configuration/GrouperPropertyConfigurer.java
+++ src/main/java/edu/hawaii/its/api/configuration/GrouperPropertyConfigurer.java
@@ -2,6 +2,7 @@ package edu.hawaii.its.api.configuration;
 
 import jakarta.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
@@ -12,6 +13,7 @@ public class GrouperPropertyConfigurer {
 
     private final Environment env;
 
+    @Autowired
     public GrouperPropertyConfigurer(Environment env) {
         this.env = env;
     }
@@ -26,12 +28,24 @@ public class GrouperPropertyConfigurer {
     }
 
     private void setOverride(GrouperClientConfig config, String key) {
-        if (isOverride(key)) {
+        String envKey = convertToEnvKey(key);
+
+        // Check for the environment variable first. If using the project's
+        // Docker container, GROUPERCLIENT_WEBSERVICE_PASSWORD may be set.
+        String envValue = System.getenv(envKey);
+        if (envValue != null) {
+            config.propertiesOverrideMap().put(key, envValue);
+        } else if (isOverride(key)) {
             config.propertiesOverrideMap().put(key, env.getProperty(key));
         }
     }
 
-    // Checks to see if override exists
+    // Helper method to convert property key to environment variable name.
+    private String convertToEnvKey(String key) {
+        return key.replace('.', '_').toUpperCase();
+    }
+
+    // Check to see an override exists.
     private boolean isOverride(String key) {
         return env.containsProperty(key);
     }
