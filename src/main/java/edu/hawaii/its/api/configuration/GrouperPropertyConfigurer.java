f820cf1e1a7f334340de83b65e14e20c6c955a92
Michael Hodges <mhodgesatuh@users.noreply.github.com>
2024-08-27 20:26:09 -1000

    Fix GrouperPropertyConfigurer and its unit test (#587)

--- src/main/java/edu/hawaii/its/api/configuration/GrouperPropertyConfigurer.java
+++ src/main/java/edu/hawaii/its/api/configuration/GrouperPropertyConfigurer.java
@@ -2,51 +2,35 @@ package edu.hawaii.its.api.configuration;
 
 import jakarta.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import edu.internet2.middleware.grouperClient.util.GrouperClientConfig;
 
 @Configuration
 public class GrouperPropertyConfigurer {
 
-    private final Environment env;
+    @Value("${grouperClient.webService.url:}")
+    private String webServiceUrl;
 
-    @Autowired
-    public GrouperPropertyConfigurer(Environment env) {
-        this.env = env;
-    }
+    @Value("${grouperClient.webService.login:}")
+    private String webServiceLogin;
+
+    @Value("${grouperClient.webService.password:}")
+    private String webServicePassword;
 
     @PostConstruct
     public void init() {
         GrouperClientConfig config = GrouperClientConfig.retrieveConfig();
 
-        setOverride(config, "grouperClient.webService.url");
-        setOverride(config, "grouperClient.webService.login");
-        setOverride(config, "grouperClient.webService.password");
+        setOverride(config, "grouperClient.webService.url", webServiceUrl);
+        setOverride(config, "grouperClient.webService.login", webServiceLogin);
+        setOverride(config, "grouperClient.webService.password", webServicePassword);
     }
 
-    private void setOverride(GrouperClientConfig config, String key) {
-        String envKey = convertToEnvKey(key);
-
-        // Check for the environment variable first. If using the project's
-        // Docker container, GROUPERCLIENT_WEBSERVICE_PASSWORD may be set.
-        String envValue = System.getenv(envKey);
-        if (envValue != null) {
-            config.propertiesOverrideMap().put(key, envValue);
-        } else if (isOverride(key)) {
-            config.propertiesOverrideMap().put(key, env.getProperty(key));
+    private void setOverride(GrouperClientConfig config, String key, String value) {
+        if (value != null) {
+            config.propertiesOverrideMap().put(key, value);
         }
-    }
-
-    // Helper method to convert property key to environment variable name.
-    private String convertToEnvKey(String key) {
-        return key.replace('.', '_').toUpperCase();
-    }
-
-    // Check to see an override exists.
-    private boolean isOverride(String key) {
-        return env.containsProperty(key);
     }
 }
