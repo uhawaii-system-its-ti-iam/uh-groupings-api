7d2e788814bbaff718397112fe8aec84af1acbd6
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2024-11-27 15:44:10 -1000

    Implement groupingOwnerService Test class (#621)

--- src/test/java/edu/hawaii/its/api/wrapper/FindAttributesResultsTest.java
+++ src/test/java/edu/hawaii/its/api/wrapper/FindAttributesResultsTest.java
@@ -5,26 +5,27 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import edu.hawaii.its.api.configuration.GroupingsTestConfiguration;
+import edu.hawaii.its.api.configuration.SpringBootWebApplication;
 import edu.hawaii.its.api.util.JsonUtil;
 import edu.hawaii.its.api.util.PropertyLocator;
 
 import edu.internet2.middleware.grouperClient.ws.beans.WsFindAttributeDefNamesResults;
 
+@ActiveProfiles("localTest")
+@SpringBootTest(classes = { SpringBootWebApplication.class })
 public class FindAttributesResultsTest {
-    private PropertyLocator propertyLocator;
 
-    @BeforeEach
-    public void beforeEach() throws Exception {
-        propertyLocator = new PropertyLocator("src/test/resources", "grouper.test.properties");
-    }
+    @Autowired
+    private GroupingsTestConfiguration groupingsTestConfiguration;
 
     @Test
     public void test() {
-        String json = propertyLocator.find("ws.attribute.def.name.results.success");
-        WsFindAttributeDefNamesResults wsFindAttributeDefNamesResults =
-                JsonUtil.asObject(json, WsFindAttributeDefNamesResults.class);
-        FindAttributesResults findAttributesResults = new FindAttributesResults(wsFindAttributeDefNamesResults);
+        FindAttributesResults findAttributesResults = groupingsTestConfiguration.attributeDefNameResultsSuccessTestData();
         assertNotNull(findAttributesResults);
         assertEquals("SUCCESS", findAttributesResults.getResultCode());
     }
