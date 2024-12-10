b2e858b70cd7ca2e04ecd30ba245ac6393cabab5
gitCarrot <96459232+gitCarrot@users.noreply.github.com>
2024-12-10 11:04:46 -1000

    Implement functions for mocking all the grouper results of OOTB tests class with the JSON file for each active profile (#623)

--- src/test/java/edu/hawaii/its/api/configuration/GroupingsTestConfiguration.java
+++ src/test/java/edu/hawaii/its/api/configuration/GroupingsTestConfiguration.java
@@ -1,10 +1,13 @@
 package edu.hawaii.its.api.configuration;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 
 import edu.hawaii.its.api.groupings.GroupingSyncDestination;
+import edu.hawaii.its.api.type.OotbActiveProfile;
 import edu.hawaii.its.api.util.JsonUtil;
 import edu.hawaii.its.api.util.PropertyLocator;
 import edu.hawaii.its.api.wrapper.AddMemberResult;
@@ -50,6 +53,10 @@ public class GroupingsTestConfiguration {
     private <T> T getWsResultTestData(String propertyName, Class<T> type) {
         String json = propertyLocator.find(propertyName);
         return JsonUtil.asObject(json, type);
+    }
+
+    public List<OotbActiveProfile> ootbActiveProfilesTestData() {
+        return JsonUtil.asListFromFile("ootb.active.user.profiles.json", OotbActiveProfile.class);
     }
 
     public HasMembersResults hasMemberResultsIsMembersUidTestData() {
