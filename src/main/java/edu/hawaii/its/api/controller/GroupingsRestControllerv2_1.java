6fb136747dae30244f9e2c86b6617fdee0494d96
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2025-09-19 11:38:11 -1000

    Remove deprecated code

--- src/main/java/edu/hawaii/its/api/controller/GroupingsRestControllerv2_1.java
+++ src/main/java/edu/hawaii/its/api/controller/GroupingsRestControllerv2_1.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -702,7 +703,9 @@ public class GroupingsRestControllerv2_1 {
     @GetMapping(value = "/members/memberships/count")
     @ResponseBody
     public ResponseEntity<Integer> getNumberOfMemberships(@RequestHeader(CURRENT_USER_KEY) String currentUser) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
         logger.info("Entered REST getNumberOfMemberships...");
+        logger.info("HERE: username: " + username);
         return ResponseEntity
                 .ok()
                 .body(membershipService.numberOfMemberships(currentUser));
