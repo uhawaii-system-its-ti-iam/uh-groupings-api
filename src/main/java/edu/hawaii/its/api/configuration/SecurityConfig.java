e781a8de3381c7d6c47ebcfda980659aebdbb39f
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2025-09-25 16:26:34 -1000

    Update GroupingsRestController

--- src/main/java/edu/hawaii/its/api/configuration/SecurityConfig.java
+++ src/main/java/edu/hawaii/its/api/configuration/SecurityConfig.java
@@ -13,8 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.*;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
