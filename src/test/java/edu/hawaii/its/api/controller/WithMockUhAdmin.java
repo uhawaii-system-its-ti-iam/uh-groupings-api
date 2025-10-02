e23ed69998e6081c083acc095464abbef25bcbbc
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2025-10-02 10:37:48 -1000

    Add SecurityConfigTest

--- src/test/java/edu/hawaii/its/api/controller/WithMockUhAdmin.java
+++ src/test/java/edu/hawaii/its/api/controller/WithMockUhAdmin.java
@@ -1,6 +1,7 @@
 package edu.hawaii.its.api.controller;
 
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -8,8 +9,12 @@ import java.lang.annotation.RetentionPolicy;
  * Mocks a logged-in user with ADMIN privileges.
  */
 @Retention(RetentionPolicy.RUNTIME)
+<<<<<<< HEAD
 @WithMockUser(
         username = "test_admin_user",
         authorities = {"ROLE_ADMINISTRATOR", "ROLE_UH"}
 )
+=======
+@WithSecurityContext(factory = WithMockUhAdminSecurityContextFactory.class)
+>>>>>>> 98484f84a (Add SecurityConfigTest)
 public @interface WithMockUhAdmin {}
--- src/test/java/edu/hawaii/its/api/controller/WithMockUhOwner.java
+++ src/test/java/edu/hawaii/its/api/controller/WithMockUhOwner.java
@@ -1,15 +1,13 @@
 package edu.hawaii.its.api.controller;
 
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Mocks a logged-in user with standard TEST_USER privileges.
+ * Mocks a logged-in user with standard OWNER privileges.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@WithMockUser(
-        username = "test_user",
-        authorities = {"ROLE_OWNER", "ROLE_UH"}
-)
+@WithSecurityContext(factory = WithMockUhOwnerSecurityContextFactory.class)
 public @interface WithMockUhOwner {}
